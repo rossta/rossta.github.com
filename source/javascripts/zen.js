90c220e2200e940ee90ae0a4e7827b7ca39909b7
Ross Kaffenberger <rosskaff@gmail.com>
2015-08-18 05:42:39 -0400

    Change some urls from http to https

--- source/javascripts/zen.js
+++ source/javascripts/zen.js
@@ -21,7 +21,7 @@ Zen = (function($) {
     self = this;
     // url  = 'http://localhost:3000/says';
     // url  = 'https://github.com/zen';
-    url  = 'http://zenkaffe.herokuapp.com/says';
+    url  = 'https://zenkaffe.herokuapp.com/says';
     return $.get(url, function(enlightenment) {
       self.setEnlightenment(enlightenment);
     }).then(function() {
