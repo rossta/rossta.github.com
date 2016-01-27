335f898af4d26a8beb10ec24cac6602acb43dc1d
Ross Kaffenberger <rosskaff@gmail.com>
2016-01-27 12:38:36 -0500

    Reconfigure bower integration with sprockets to avoid building entire bower_components tree

--- config.rb
+++ config.rb
@@ -94,7 +94,7 @@ end
 
 compass_config do |config|
   # Require any additional compass plugins here.
-  config.add_import_path "bower_components/foundation/scss"
+  config.add_import_path "../bower_components/foundation/scss"
 
   # Set this to the root of your project when deployed:
   config.http_path = "/"
