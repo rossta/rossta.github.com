15227839bb2caf91d59935aff07f22e8ebf9a4bb
Ross Kaffenberger <rosskaff@gmail.com>
2016-04-19 17:33:38 -0400

    Site updated to a68d0ad

---
+++ assets/javascripts/serviceworker-6a084aa9.js
@@ -0,0 +1,174 @@
+'use strict';
+
+const version = 'v20160419';
+const offlineResources = [
+  '/',
+  '/offline.html'
+];
+
+const ignoreFetch = [
+  /https?:\/\/api.mixpanel.com\//,
+  /https?:\/\/api.segment.io\//,
+  /https?:\/\/in.getclicky.com\//,
+  /https?:\/\/zenkaffe.herokuapp.com\//,
+  /https?:\/\/p.typekit.net\//,
+  /\/__rack\//,
+];
+
+const offlineImage = '<svg width="400" height="300" role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>';
+
+//////////
+// Install
+//////////
+function onInstall(event) {
+  log('install event in progress.');
+
+  event.waitUntil(updateStaticCache());
+}
+
+function updateStaticCache() {
+  return caches
+    .open(cacheKey('offline'))
+    .then((cache) => {
+      return cache.addAll(offlineResources);
+    })
+    .then(() => {
+      log('installation complete!');
+    });
+}
+
+////////
+// Fetch
+////////
+function onFetch(event) {
+  const request = event.request;
+
+  if (shouldAlwaysFetch(request)) {
+    event.respondWith(networkedOrOffline(request));
+    return;
+  }
+
+  if (shouldFetchAndCache(request)) {
+    event.respondWith(networkedOrCached(request));
+    return;
+  }
+
+  event.respondWith(cachedOrNetworked(request));
+}
+
+function networkedOrCached(request) {
+  return networkedAndCache(request)
+    .catch(() => { return cachedOrOffline(request) });
+}
+
+// Stash response in cache as side-effect of network request
+function networkedAndCache(request) {
+  return fetch(request)
+    .then((response) => {
+      var copy = response.clone();
+      caches.open(cacheKey('resources'))
+        .then((cache) => {
+          cache.put(request, copy);
+        });
+
+      log("(network: cache write)", request.method, request.url);
+      return response;
+    });
+}
+
+function cachedOrNetworked(request) {
+  return caches.match(request)
+    .then((response) => {
+      log(response ? '(cached)' : '(network: cache miss)', request.method, request.url);
+      return response ||
+        networkedAndCache(request)
+          .catch(() => { return offlineResponse(request) });
+    });
+}
+
+function networkedOrOffline(request) {
+  return fetch(request)
+    .then((response) => {
+      log('(network)', request.method, request.url);
+      return response;
+    })
+    .catch(() => {
+      return offlineResponse(request);
+    });
+}
+
+function cachedOrOffline(request) {
+  return caches
+    .match(request)
+    .then((response) => {
+      return response || offlineResponse(request);
+    });
+}
+
+function offlineResponse(request) {
+  log('(offline)', request.method, request.url);
+  if (~request.headers.get('Accept').indexOf('image')) {
+    return new Response(offlineImage, { headers: { 'Content-Type': 'image/svg+xml' }});
+  } else {
+    return caches.match('/offline.html');
+  }
+}
+
+///////////
+// Activate
+///////////
+function onActivate(event) {
+  log('activate event in progress.');
+  event.waitUntil(removeOldCache());
+}
+
+function removeOldCache() {
+  return caches
+    .keys()
+    .then((keys) => {
+      return Promise.all( // We return a promise that settles when all outdated caches are deleted.
+                         keys
+                         .filter((key) => {
+                           return !key.startsWith(version); // Filter by keys that don't start with the latest version prefix.
+                         })
+                         .map((key) => {
+                           return caches.delete(key); // Return a promise that's fulfilled when each outdated cache is deleted.
+                         })
+                        );
+    })
+    .then(() => {
+      log('removeOldCache completed.');
+    });
+}
+
+function cacheKey() {
+  return [version, ...arguments].join(':');
+}
+
+function log() {
+  if (developmentMode()) {
+    console.log("SW:", ...arguments);
+  }
+}
+
+function shouldAlwaysFetch(request) {
+  return __DEVELOPMENT__ ||
+    request.method !== 'GET' ||
+      ignoreFetch.some(regex => request.url.match(regex));
+}
+
+function shouldFetchAndCache(request) {
+  return ~request.headers.get('Accept').indexOf('text/html');
+}
+
+function developmentMode() {
+  return __DEVELOPMENT__ || __DEBUG__;
+}
+
+log("Hello from ServiceWorker land!", version);
+
+self.addEventListener('install', onInstall);
+
+self.addEventListener('fetch', onFetch);
+
+self.addEventListener("activate", onActivate);
