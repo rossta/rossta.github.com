4b264e5a1e701b258c33a79f1162fa8265ea4346
Ross Kaffenberger <rosskaff@gmail.com>
2016-04-20 16:14:34 -0400

    Added logging for service worker registration; remove svg string

--- source/assets/javascripts/enhance.js
+++ source/assets/javascripts/enhance.js
@@ -3,10 +3,27 @@ if('serviceWorker' in navigator) {
   log('service worker registration in progress.');
   navigator.serviceWorker.register('/serviceworker.js', {
     scope: '/'
-  }).then(function() {
-    log('service worker registration complete.');
-  }, function() {
-    log('service worker registration failure.');
+  }).then(function (registration) {
+    log('service worker is registered!');
+      var serviceWorker;
+      if (registration.installing) {
+          serviceWorker = registration.installing;
+          log('service worker is now installing');
+      } else if (registration.waiting) {
+          serviceWorker = registration.waiting;
+          log('service worker is now waiting');
+      } else if (registration.active) {
+          serviceWorker = registration.active;
+          log('service worker is now active');
+      }
+      if (serviceWorker) {
+        log('service worker state:', serviceWorker.state);
+        serviceWorker.addEventListener('statechange', function (e) {
+          log('service worker state:', serviceWorker.state);
+        });
+      }
+  }).catch (function (error) {
+    log('service worker failed to register.', error);
   });
 } else {
   log('service worker is not supported.');
