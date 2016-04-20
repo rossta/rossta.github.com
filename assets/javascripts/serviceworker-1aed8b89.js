7eae9b878bbbf0eb22de8e4b95c517311e60a791
Ross Kaffenberger <rosskaff@gmail.com>
2016-04-20 15:17:49 -0400

    Site updated to efbb4d4

--- assets/javascripts/serviceworker-1aed8b89.js
+++ assets/javascripts/serviceworker-1aed8b89.js
@@ -3,7 +3,8 @@
 const version = 'v20160419';
 const offlineResources = [
   '/',
-  '/offline.html'
+  '/offline.html',
+  '/offline-c5946202.svg'
 ];
 
 const ignoreFetch = [
@@ -107,8 +108,8 @@ function cachedOrOffline(request) {
 
 function offlineResponse(request) {
   log('(offline)', request.method, request.url);
-  if (~request.headers.get('Accept').indexOf('image')) {
-    return new Response(offlineImage, { headers: { 'Content-Type': 'image/svg+xml' }});
+  if (request.url.match(/\.(jpg|png|gif|svg|jpeg)(\?.*)?$/)) {
+    return caches.match('/offline-c5946202.svg');
   } else {
     return caches.match('/offline.html');
   }
