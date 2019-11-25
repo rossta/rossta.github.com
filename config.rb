73dc2260dbe424b50c99838c10ba7e5875bf0e63
Ross Kaffenberger <rosskaff@gmail.com>
2019-11-25 04:48:39 -0500

    Add blog tags index and curate footer tags

--- config.rb
+++ config.rb
@@ -1,52 +1,42 @@
 # frozen_string_literal: true
+require "json"
+require_relative("./lib/custom_markdown_renderer")
 
-require 'json'
-require_relative './lib/custom_markdown_renderer'
+activate(:livereload)
+activate(:meta_tags)
 
-activate :livereload
-activate :meta_tags
 # activate :directory_indexes
-
 # Time.zone = "UTC"
-activate :external_pipeline,
-         name: :webpack,
-         command: build? ? 'yarn build:prod' : 'yarn build:dev',
-         source: '.tmp/dist',
-         latency: 1
+activate(:external_pipeline, name: :webpack, command: build? ? "yarn build:prod" : "yarn build:dev", source: ".tmp/dist", latency: 1)
 
 ###########################
 ## Blog
 ###########################
-activate :blog do |blog|
-  blog.name = 'blog'
-  blog.prefix = 'blog'
-  blog.permalink = '/{title}.html'
-  blog.sources = '{year}-{month}-{day}-{title}.html'
-  blog.layout = 'post'
-  blog.tag_template = 'tag.html'
-  blog.calendar_template = 'calendar.html'
+activate(:blog) do |blog|
+  blog.name = "blog"
+  blog.prefix = "blog"
+  blog.permalink = "/{title}.html"
+  blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.layout = "post"
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
   blog.paginate = true
   blog.per_page = 25
-  blog.default_extension = '.md'
-  blog.custom_collections = {
-    series: {
-      link: '/series/{series}.html',
-      template: 'series.html'
-    }
-  }
+  blog.default_extension = ".md"
+  blog.custom_collections = {series: {link: "/series/{series}.html", template: "series.html"}}
 end
 
 ###########################
 ## Talks
 ###########################
-activate :blog do |blog|
-  blog.name = 'talks'
-  blog.prefix = 'talks'
-  blog.permalink = '/{title}.html'
-  blog.taglink = 'tags/{tag}'
-  blog.sources = '{year}-{month}-{day}-{title}.html'
-  blog.default_extension = '.md'
-  blog.layout = 'talk'
+activate(:blog) do |blog|
+  blog.name = "talks"
+  blog.prefix = "talks"
+  blog.permalink = "/{title}.html"
+  blog.taglink = "tags/{tag}"
+  blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.default_extension = ".md"
+  blog.layout = "talk"
   blog.paginate = true
   blog.per_page = 25
 end
@@ -54,73 +44,58 @@ end
 ###########################
 ## Projects
 ###########################
-activate :blog do |blog|
-  blog.name = 'projects'
-  blog.prefix = 'projects'
-  blog.permalink = '/{title}.html'
-  blog.taglink = 'tags/{tag}'
-  blog.sources = '{year}-{month}-{day}-{title}.html'
-  blog.default_extension = '.md'
-  blog.layout = 'project'
+activate(:blog) do |blog|
+  blog.name = "projects"
+  blog.prefix = "projects"
+  blog.permalink = "/{title}.html"
+  blog.taglink = "tags/{tag}"
+  blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.default_extension = ".md"
+  blog.layout = "project"
   blog.paginate = true
   blog.per_page = 25
 end
 
-set :markdown_engine, :redcarpet
-set :markdown,
-    layout_engine: :erb,
-    no_intra_emphasis: true,
-    fenced_code_blocks: true,
-    autolink: true,
-    disable_indented_code_blocks: true,
-    smartypants: true,
-    lax_spacing: true,
-    renderer: CustomMarkdownRenderer
-
-set :css_dir, 'assets/stylesheets'
-set :js_dir, 'assets/javascripts'
-set :images_dir, 'assets/images'
-
-ignore 'assets/stylesheets/*'
-ignore 'assets/javascripts/*'
+set(:markdown_engine, :redcarpet)
+set(:markdown, layout_engine: :erb, no_intra_emphasis: true, fenced_code_blocks: true, autolink: true, disable_indented_code_blocks: true, smartypants: true, lax_spacing: true, renderer: CustomMarkdownRenderer)
+set(:css_dir, "assets/stylesheets")
+set(:js_dir, "assets/javascripts")
+set(:images_dir, "assets/images")
+ignore("assets/stylesheets/*")
+ignore("assets/javascripts/*")
 
 # Disabled in favor of Webpack hashing
 # activate :asset_hash
-
 # Build-specific configuration
-configure :build do
-  set :trailing_slash, false
-
-  set :protocol, 'https'
-  set :host, 'rossta.net'
-  set :port, nil
-  set :google_analytics_id, 'UA-16458563-2'
-  set :mailchimp_form_id, '96030b0bda'
-  set :segmentio_id, 'NdBtrprkAGAjQryMShljRdVf90saElAU'
-
-  activate :gzip, exts: %w[.js .css .html .htm .svg .ttf .otf .woff .eot]
+configure(:build) do
+  set(:trailing_slash, false)
+  set(:protocol, "https")
+  set(:host, "rossta.net")
+  set(:port, nil)
+  set(:google_analytics_id, "UA-16458563-2")
+  set(:mailchimp_form_id, "96030b0bda")
+  set(:segmentio_id, "NdBtrprkAGAjQryMShljRdVf90saElAU")
+  activate(:gzip,   exts: %w[.js .css .html .htm .svg .ttf .otf .woff .eot])
 end
 
-configure :development do
-  set :protocol, 'http'
-  set :host, 'localhost'
-  set :port, 4567
-
-  set :google_analytics_id, 'UA-xxxxxxxx-x'
-  set :mailchimp_form_id, 'a57e354058'
-  set :segmentio_id, '7KlQZWWPWr2MDj4pWepIF7O95JPZ9wfp'
+configure(:development) do
+  set(:protocol, "http")
+  set(:host, "localhost")
+  set(:port, 4567)
+  set(:google_analytics_id, "UA-xxxxxxxx-x")
+  set(:mailchimp_form_id, "a57e354058")
+  set(:segmentio_id, "7KlQZWWPWr2MDj4pWepIF7O95JPZ9wfp")
 end
 
 ###
 # Page options, layouts, aliases and proxies
 ###
-
 # Per-page layout changes:
 #
 # With no layout
-page '/playground.html', layout: false
-page '/feed.xml', layout: false
-page '/sitemap.xml', layout: false
+page("/playground.html", layout: false)
+page("/feed.xml", layout: false)
+page("/sitemap.xml", layout: false)
 
 #
 # With alternative layout
@@ -130,43 +105,41 @@ page '/sitemap.xml', layout: false
 # with_layout :admin do
 #   page "/admin/*"
 # end
-
 # Proxy (fake) files
 # page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
 #   @which_fake_page = "Rendering a fake page with a variable"
 # end
-
 ###
 # Helpers
 ###
-
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
-
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
 #     "Helping"
 #   end
 # end
-
+# rubocop:disable
 helpers do
+
   def page_title
     yield_content(:title)
   end
 
   def page_header(title, summary = nil)
-    partial 'partials/page_header', locals: { title: title, summary: summary }
+    partial("partials/page_header",     locals: {title: title, summary: summary})
   end
 
   def section
-    (yield_content(:section) || title || '')
+    (yield_content(:section) || title || "")
   end
 
   def webpack_manifest
-    manifest_path = '.tmp/dist/assets/manifest.json'
+    manifest_path = ".tmp/dist/assets/manifest.json"
+
     unless File.exist?(manifest_path)
-      warn 'Could not read manifest json!!'
+      warn("Could not read manifest json!!")
       return {}
     end
 
@@ -174,63 +147,59 @@ helpers do
   end
 
   def stylesheet_pack_tag(*names)
-    names.map { |name| "<link href='#{webpack_manifest[name + '.css']}' rel='stylesheet'></link>" }.join('')
+    names.map { |name| "<link href='#{webpack_manifest[name + ".css"]}' rel='stylesheet'></link>" }.join("")
   end
 
   def javascript_pack_tag(*names)
-    names.map { |name| "<script src='#{webpack_manifest[name + '.js']}'></script>" }.join('')
+    names.map { |name| "<script src='#{webpack_manifest[name + ".js"]}'></script>" }.join("")
   end
 
   def to_url(opts = {})
     Addressable::URI.new({
       scheme: config[:protocol],
       host: config[:host],
-      port: build? ? nil : config[:port]
+      port: build? ? nil : config[:port],
     }.merge(opts))
   end
 
   def current_url
     path = current_page.path
-    path = '/' if homepage?
-    to_url(path: path)
+    path = "/" if homepage?
+    to_url(    path: path)
   end
 
   def homepage?
-    current_page.path == 'index.html'
+    current_page.path == "index.html"
   end
 
   def image_url(source)
-    to_url(path: image_path(source))
+    to_url(    path: image_path(source))
   end
 
   def email_url
-    'mailto:ross@rossta.net'
+    "mailto:ross@rossta.net"
   end
 
   def signup_form_url
-    Addressable::URI.new(
-      scheme: nil,
-      host: 'rossta.us6.list-manage.com',
-      path: '/subscribe/post',
-      query_values: {
-        u: '8ce159842b5c98cecb4ebdf16',
-        id: config[:mailchimp_form_id]
-      }
-    )
+    Addressable::URI.new(    scheme: nil,     host: "rossta.us6.list-manage.com",     path: "/subscribe/post",     query_values: {u: "8ce159842b5c98cecb4ebdf16", id: config[:mailchimp_form_id]})
   end
 
   def tweet_link_to(text, params = {}, options = {})
-    uri = Addressable::URI.parse('https://twitter.com/intent/tweet')
+    uri = Addressable::URI.parse("https://twitter.com/intent/tweet")
     uri.query_values = params
-    link_to text, uri, options.merge(target: '_blank', rel: 'noopener')
+    link_to(text, uri, options.merge(    target: "_blank",     rel: "noopener"))
   end
 
   def top_tags
-    blog('blog').tags.sort_by { |_t, a| -a.count }
+    blog("blog").tags.sort_by { |_t, a| -a.count }.to_h
+  end
+
+  def top_curated_tags
+    top_tags.keys & %w[Rails Vue Ruby Webpack JavaScript]
   end
 
   def top_articles
-    blog('blog').articles.select { |a| a.data[:popular] }.sort_by { |a| a.data[:popular] }
+    blog("blog").articles.select { |a| a.data[:popular] }.sort_by { |a| a.data[:popular] }
   end
 
   Series = Struct.new(:id, :title, :summary)
@@ -238,25 +207,25 @@ helpers do
   def blog_series
     [
       [
-        'PDF Viewer',
-        'Building a PDF Viewer with Vue.js',
-        'A demo app to render PDFs using PDF.js, Vue, Webpack, and the canvas element'
+        "PDF Viewer",
+        "Building a PDF Viewer with Vue.js",
+        "A demo app to render PDFs using PDF.js, Vue, Webpack, and the canvas element",
       ],
       [
-        'Connect Four',
-        'Building Connect Four with Vue.js and Phoenix',
-        'Tackling multi-player game rendering, animation, and realtime interaction'
+        "Connect Four",
+        "Building Connect Four with Vue.js and Phoenix",
+        "Tackling multi-player game rendering, animation, and realtime interaction",
       ],
       [
-        'Service Worker',
-        'Progressive Web Apps on Rails',
-        'Leveraging the powerful JavaScript API for Progressive Web Apps'
+        "Service Worker",
+        "Progressive Web Apps on Rails",
+        "Leveraging the powerful JavaScript API for Progressive Web Apps",
       ],
       [
-        'Enumerable',
+        "Enumerable",
         "Exploring Ruby's Enumerable",
-        'Working with collections and sequences in Ruby'
-      ]
+        "Working with collections and sequences in Ruby",
+      ],
     ].map { |data| Series.new(*data) }
   end
 
@@ -270,19 +239,19 @@ helpers do
 
   def convertkit_tag_value(tag)
     {
-      'JavaScript' => '733959',
-      'Vue' => '733963',
-      'Ruby' => '733960',
-      'Rails' => '733966',
-      'Webpack' => '733964',
-      'Service Worker' => '733965',
-      'Node' => '733969',
-      'Elixir' => '733970'
+      "JavaScript" => "733959",
+      "Vue" => "733963",
+      "Ruby" => "733960",
+      "Rails" => "733966",
+      "Webpack" => "733964",
+      "Service Worker" => "733965",
+      "Node" => "733969",
+      "Elixir" => "733970",
     }[tag]
   end
 
   def convertkit_campaign
-    explicit_page_tags.first || 'Homepage'
+    explicit_page_tags.first || "Homepage"
   end
 
   def nozen?
