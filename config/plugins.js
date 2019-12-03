abe09b7b91878d154b2e00e367b39d5bfbce7482
Ross Kaffenberger <rosskaff@gmail.com>
2019-12-02 21:45:04 -0500

    Add and apply purgecss
    Before:
    
    $ ls -lah build/assets/css
    total 2072
    drwxr-xr-x  6 ross  staff   192B Dec  2 21:44 .
    drwxr-xr-x  6 ross  staff   192B Dec  2 21:44 ..
    -rw-r--r--  1 ross  staff   868K Dec  2 21:44 app.chunk.cd4d8babd6ab0d5561c9.css
    -rw-r--r--  1 ross  staff    91K Dec  2 21:44 app.chunk.cd4d8babd6ab0d5561c9.css.gz
    -rw-r--r--  1 ross  staff    10K Dec  2 21:44 convertkit-form.chunk.d709247305ad342cada9.css
    -rw-r--r--  1 ross  staff   1.7K Dec  2 21:44 convertkit-form.chunk.d709247305ad342cada9.css.gz
    
    After:
    
    $ ls -lah build/assets/css
    total 80
    drwxr-xr-x  6 ross  staff   192B Dec  2 21:41 .
    drwxr-xr-x  6 ross  staff   192B Dec  2 21:41 ..
    -rw-r--r--  1 ross  staff    17K Dec  2 21:41 app.chunk.86cb6f4c57b1b2a5d8b7.css
    -rw-r--r--  1 ross  staff   4.8K Dec  2 21:41 app.chunk.86cb6f4c57b1b2a5d8b7.css.gz
    -rw-r--r--  1 ross  staff   7.9K Dec  2 21:39 convertkit-form.chunk.63d70506d0ee4d8abc90.css
    -rw-r--r--  1 ross  staff   1.4K Dec  2 21:39 convertkit-form.chunk.63d70506d0ee4d8abc90.css.gz
    
    Major reduction to app css!

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack');
-const path = require('path');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const  { CleanWebpackPlugin } = require('clean-webpack-plugin');
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,12 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
+    purgecss({
+      content: ['./**/*.erb'],
+    }),
   ]
 }
