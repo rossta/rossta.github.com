de769dac46db40c5595d0f0f71eb509b8c4f9da3
Ross Kaffenberger <rosskaff@gmail.com>
2020-01-13 17:46:57 -0500

    Update legibility of font

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,17 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./**/*.erb', './**/*.rb'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  whitelist: ['blockquote', 'li', 'pre', 'code', 'figcaption'],
+  whitelistPatterns: [/hljs/],
+})
 
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-    purgecss({
-      content: ['./**/*.erb', './**/*.rb'],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      whitelist: ['blockquote', 'li', 'pre', 'code', 'figcaption'],
-      whitelistPatterns: [/hljs/],
-    }),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
