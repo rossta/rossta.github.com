27054c88376e37c69ef385346705f409adb5346b
Ross Kaffenberger <rosskaff@gmail.com>
2020-01-19 10:43:39 -0500

    Change async form input type

--- source/assets/javascripts/enhance.js
+++ source/assets/javascripts/enhance.js
@@ -1,26 +1,32 @@
-import debug from 'debug';
+import debug from 'debug'
 
-const log = debug('app:enhance');
+const log = debug('app:enhance')
 
 // ServiceWorker is a progressive technology. Ignore unsupported browsers
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach(registration => registration.unregister());
-  });
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(registration => registration.unregister())
+  })
 } else {
-  log('service worker is not supported.');
+  log('service worker is not supported.')
 }
-
 
 window.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.top-bar-menu-button').forEach(el => {
-    el.addEventListener('click', (event) => {
-      event.stopPropagation();
-      toggleClass(document.querySelector('.top-bar-section'), 'hidden');
-      el.querySelectorAll('.icon').forEach(ic => toggleClass(ic, 'hidden'));
+    el.addEventListener('click', event => {
+      event.stopPropagation()
+      toggleClass(document.querySelector('.top-bar-section'), 'hidden')
+      el.querySelectorAll('.icon').forEach(ic => toggleClass(ic, 'hidden'))
     })
-  });
-});
+  })
+
+  const slideInForm = document.querySelector('.formkit-slide-in')
+  if (slideInForm) {
+    log('slide in form detected')
+  } else {
+    log('slide in form NOT detected')
+  }
+})
 
 function toggleClass(el, className) {
   if (el.classList.contains(className)) {
@@ -29,3 +35,33 @@ function toggleClass(el, className) {
     el.classList.add(className)
   }
 }
+
+const targetNode = document.querySelector('body')
+
+// Options for the observer (which mutations to observe)
+const config = { childList: true }
+
+// Callback function to execute when mutations are observed
+const callback = function(mutationsList, observer) {
+  for (let mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      mutation.addedNodes.forEach((node) => {
+        if (node.classList && node.classList.contains('formkit-slide-in')) {
+          log('formkit slide in', node)
+          const input = node.querySelector('input[name=\'email_address\']')
+          input.type = 'email'
+          log('input', input)
+        }
+      })
+    }
+  }
+}
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback)
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config)
+
+// Later, you can stop observing
+// observer.disconnect()
